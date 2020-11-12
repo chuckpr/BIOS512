f8307e08e7340e72bba212d0272166fa613b64ed
chuckpr <cpeperanney@agbiome.com>
2020-11-12 09:47:19 -0500

    let devtools install deps

--- install.R
+++ install.R
@@ -1,6 +1,5 @@
 devtools::install_github("thomasp85/patchwork")
 install.packages("palmerpenguins", repos="https://cran.rstudio.com")
 install.packages("ggrepel", repos="https://cran.rstudio.com")
-install.packages("igraph", repos="https://cran.rstudio.com")
-devtools::install_github('thomasp85/tidygraph', dep = FALSE)
-devtools::install_github('thomasp85/ggraph', dep = FALSE)
+devtools::install_github('thomasp85/tidygraph')
+devtools::install_github('thomasp85/ggraph')
